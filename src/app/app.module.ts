c5b17da6e39ec066581c77152e7406a31e19a2ba
Santiago Losada Borrajo <santiago.losadaborrajo@plexus.local>
2018-04-16 17:21:19 +0200

    Fixes initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RoutesModule } from './routes.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { RoutesModule } from './/routes.module';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { RoutesModule } from './/routes.module';
   ],
   imports: [
     BrowserModule,
-    RoutesModule
+    RoutesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes.module.ts
+++ src/app/routes.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
-  {
-    path: '' , redirectTo: 'inicio', pathMatch: 'full'
-  }
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent}
 ];
 
 @NgModule({
-  exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes)],
-  declarations: []
+  imports: [
+    [ RouterModule.forRoot(routes) ],
+    CommonModule
+  ],
+  exports: [ RouterModule ]
 })
+
 export class RoutesModule { }
