6584232af178ddc87ff9f449916ab33b2c1efa02
Jason Danilo Franco Quintero <jason.francoquintero@plexus.local>
2018-04-17 12:39:02 +0200

    update form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {FormService } from '../form.service';
+import { User } from '../user';
+import { UsersService } from '../users.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  user = new User;
   countries: any[];
-  constructor(private formService: FormService) { }
+  constructor(private formService: FormService, public usersService: UsersService) { }
   ngOnInit() {
     this.getCoutries();
   }
   getCoutries(): void {
     this.formService.getCountries().subscribe(countries => this.countries = countries);
   }
-
+  addUser(): void {
+    this.usersService.add(this.user);
+  }
 }
