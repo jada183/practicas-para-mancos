2b710f099c98be5834531d761937cca6079c3323
Santiago Losada Borrajo <santiago.losadaborrajo@plexus.local>
2018-04-17 12:34:18 +0200

    Update list users

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
+
 @Injectable()
 export class UsersService {
-  users: User[];
+  users: User[] = [
+    { name: 'Santiago', lastName: 'Losada Borrajo', country: 'España', email: 'email@string.com' },
+    { name: 'Jason', lastName: 'Franco Quinteiro', country: 'España', email: 'yeah@string.com' }
+  ];
+
   constructor() { }
+
   add(user: User): void {
     this.users.push(user);
+  }
+
+  delete(user: User): void {
+    let numb: number;
+    for (const oneUser of this.users) {
+      if (oneUser.name === user.name) {
+        numb = this.users.indexOf(oneUser);
+      }
+    }
+    this.users.splice(numb, 1);
   }
 }
